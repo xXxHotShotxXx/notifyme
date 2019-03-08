c61ba3be59d53a5414039146d0c8df502fbc0551
xXxHotShotxXx <hotshot.twitch@gmail.com>
2019-03-08 16:17:33 -0500

    updater

---
+++ client_config.py
@@ -0,0 +1,6 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'None'
+    APP_NAME = 'notifyme'
+    COMPANY_NAME = 'Hot-Gamez'
+    UPDATE_URLS = ['https://hot-gamez.com']
+    MAX_DOWNLOAD_RETRIES = 3
