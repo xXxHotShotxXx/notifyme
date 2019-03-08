48c075fc0ec01bf0ced5b25d695ff9ec95109703
xXxHotShotxXx <hotshot.twitch@gmail.com>
2019-03-08 16:30:38 -0500

    updated keys

--- client_config.py
+++ client_config.py
@@ -1,5 +1,5 @@
 class ClientConfig(object):
-    PUBLIC_KEY = 'None'
+    PUBLIC_KEY = 'HbYnchhQiZy2HKbDpauf6gJhriyDXrzSXAylGibCqQY'
     APP_NAME = 'notifyme'
     COMPANY_NAME = 'Hot-Gamez'
     UPDATE_URLS = ['https://hot-gamez.com']
